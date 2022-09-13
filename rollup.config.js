75a8dd166ee17940c0a8e97c3c8c1b87c17269e7
Dominique Müller <dominique.m.mueller@gmail.com>
2022-09-13 17:29:01 +0200

    Disable node builtin import prefix

--- rollup.config.js
+++ rollup.config.js
@@ -16,6 +16,7 @@ export default [
     plugins: [
       externals({
         builtins: true,
+        builtinsPrefix: 'strip',
         deps: true,
         devDeps: true,
         peerDeps: true,
@@ -45,6 +46,7 @@ export default [
     plugins: [
       externals({
         builtins: true,
+        builtinsPrefix: 'strip',
         deps: true,
         devDeps: true,
         peerDeps: true,
